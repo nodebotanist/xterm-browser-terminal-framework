6e127fb1779886ef3c1c321f49fef8990df1cc7e
Kassandra Perch <the@nodebotani.st>
2018-12-17 11:26:17 -0600

    Example works with xterm

--- example/scripts.js
+++ example/scripts.js
@@ -1,1 +1,5 @@
+const Terminal = require('xterm').Terminal
 const BrowserTerminal = require('../dist/BrowserTerminal')
+
+let term = new Terminal({})
+term.open(document.getElementById('#terminal'))
