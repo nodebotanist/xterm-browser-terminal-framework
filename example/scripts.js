8ecd482bfa2590b7a9ee92a466fa3b4067a6a74d
Kassandra Perch <the@nodebotani.st>
2018-12-17 11:14:49 -0600

    Set up example and renamed build file

---
+++ example/scripts.js
@@ -0,0 +1,1 @@
+const BrowserTerminal = require('../dist/BrowserTerminal')
--- example/webpack.config.js
+++ example/webpack.config.js
@@ -2,14 +2,17 @@ const path = require('path')
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: "./scripts.js",
   // Here the application starts executing
   // and webpack starts bundling
   output: {
     // options related to how webpack emits results
-    path: path.resolve(__dirname, "dist"), // string
+    path: path.resolve(__dirname), // string
     // the target directory for all output files
     // must be an absolute path (use the Node.js path module)
-    filename: "BrowserTerminal.js", // string
+    filename: "bundle.js", // string
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname)
   }
 }
