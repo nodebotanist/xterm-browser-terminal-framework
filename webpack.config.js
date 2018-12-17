8e0d710dc52e84d23576c945488ae0cf4d45abd0
Kassandra Perch <the@nodebotani.st>
2018-12-17 10:47:59 -0600

    Added webpack and config

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  // Here the application starts executing
+  // and webpack starts bundling
+  output: {
+    // options related to how webpack emits results
+    path: path.resolve(__dirname, "dist"), // string
+    // the target directory for all output files
+    // must be an absolute path (use the Node.js path module)
+    filename: "main.js", // string
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist')
+  }
+}
