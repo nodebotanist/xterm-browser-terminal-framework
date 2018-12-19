4c85f0fbfa4b6d7c1cd5a26a13dcca392bfd9f85
Kassandra Perch <the@nodebotani.st>
2018-12-19 12:00:59 -0600

    xterm-browser now extends xterm, fixes #3

--- src/dispatcher.js
+++ src/dispatcher.js
@@ -8,7 +8,6 @@ function Dispatcher() {
 util.inherits(Dispatcher, EventEmitter)
 
 Dispatcher.prototype.dispatch = function(data) {
-  console.log(data)
   this.emit(data._[0], data)
 }
 
--- src/terminal.js
+++ src/terminal.js
@@ -1,41 +1,45 @@
+const util = require('util')
+const xterm = require('xterm').Terminal
+
 const Dispatcher = require('./dispatcher')
 const Parser = require('./parser')
 const format = require('./format')
 
-function BrowserTerminal (xTermInstance) {
-  this.terminal = xTermInstance
+function BrowserTerminal (args) {
+  xterm.call(this, args)
   this.dispatcher = new Dispatcher()
   this.parser = new Parser(this.dispatcher)
   this.format = format
 
-
-  this.currentLine = ''
-  this.terminal.prompt = () => {
-    this.terminal.write(`${format.newLine()}$ `)
+  this._currentLine = ''
+  this.prompt = () => {
+    this.write(`${format.newLine()}$ `)
   }
 }
 
-BrowserTerminal.prototype.setup= function () {
-  this.terminal.write('$ ')
+util.inherits(BrowserTerminal, xterm)
 
-  this.terminal._core.register(this.terminal.addDisposableListener('key', (key, ev) => {
+BrowserTerminal.prototype.setup= function () {
+  this.write('$ ')
+
+  this._core.register(this.addDisposableListener('key', (key, ev) => {
     const printable = !ev.altKey && !ev.altGraphKey && !ev.ctrlKey && !ev.metaKey;
   
     if (ev.keyCode === 13) {
-      if(this.currentLine.length > 0) {
-        this.terminal.write(this.parser.parse(this.currentLine))
-        this.currentLine =  ''
+      if(this._currentLine.length > 0) {
+        this.write(this.parser.parse(this._currentLine))
+        this._currentLine =  ''
       }
-      this.terminal.prompt()
+      this.prompt()
     } else if (ev.keyCode === 8) {
       // Do not delete the prompt
-      if(this.currentLine.length > 0) {
-        this.terminal.write('\b \b')
-        this.currentLine = this.currentLine.substring(0, this.currentLine.length - 1)
+      if(this._currentLine.length > 0) {
+        this.write('\b \b')
+        this._currentLine = this._currentLine.substring(0, this._currentLine.length - 1)
       }
     } else if (printable) {
-      this.currentLine = this.currentLine.concat(key)
-      this.terminal.write(key)
+      this._currentLine = this._currentLine.concat(key)
+      this.write(key)
     }
   }))
   
