27ec5edef1c9fcecd442c1dd43b8e1ca4c9d6d27
nodebotanist <the@nodebotani.st>
2019-10-08 13:50:34 -0500

    Removed up and down arrows

--- src/terminal.js
+++ src/terminal.js
@@ -6,7 +6,7 @@ const Dispatcher = require('./dispatcher')
 const Parser = require('./parser')
 const format = require('./format')
 
-function BrowserTerminal (args) {
+function BrowserTerminal(args) {
   xterm.call(this, args)
   this.dispatcher = new Dispatcher()
   this.parser = new Parser(this.dispatcher)
@@ -21,29 +21,30 @@ function BrowserTerminal (args) {
 
 util.inherits(BrowserTerminal, xterm)
 
-BrowserTerminal.prototype.setup= function () {
+BrowserTerminal.prototype.setup = function () {
   this._core.register(this.onKey((key) => {
-    console.log(key )
     const printable = !key.domEvent.altKey && !key.domEvent.altGraphKey && !key.domEvent.ctrlKey && !key.domEvent.metaKey;
-  
+
     if (key.domEvent.keyCode === 13) {
-      if(this._currentLine.length > 0) {
+      if (this._currentLine.length > 0) {
         this.dispatcher.dispatch(yargs(this._currentLine))
-        this._currentLine =  ''
+        this._currentLine = ''
       }
       this.prompt()
     } else if (key.domEvent.keyCode === 8) {
       // Do not delete the prompt
-      if(this._currentLine.length > 0) {
+      if (this._currentLine.length > 0) {
         this.write('\b \b')
         this._currentLine = this._currentLine.substring(0, this._currentLine.length - 1)
       }
+    } else if (key.domEvent.code === "ArrowUp" || key.domEvent.code === "ArrowDown") {
+      // do nothing
     } else if (printable) {
       this._currentLine = this._currentLine.concat(key.key)
       this.write(key.key)
     }
   }))
-  
+
 }
 
 module.exports = {
